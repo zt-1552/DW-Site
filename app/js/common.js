f38280dce0deac1da6f6db9c03da04e8ceee6119
Yurii Kovalchuk <zt1552@gmail.com>
2019-06-10 16:09:29 +0300

    Ready 70%

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,19 @@
 ;(function($, undefined){
- 
+
+ $('#button-menu').on('click', function (){
+  console.log('Menu');
+})
+
+
+$(".img-post-footer").on("click", function () { // Отображение на весь экран
+  var img = $(this);
+  var src = img.attr('src');
+  $("#img-post-footer-big").attr('src', src);
+  $("#modal-window").show();
+});
+
+$("#img-post-footer-big").on("click", function () { // Кнопка закрыть
+	$("#modal-window").hide();
+});
 
 })(jQuery);
