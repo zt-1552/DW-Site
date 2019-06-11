f8e579f8bd3892c6c4bcea8c6e0f2ad8fa3638aa
Yurii Kovalchuk <zt1552@gmail.com>
2019-06-11 17:49:10 +0300

    Final

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,29 +1,29 @@
 ;(function($, undefined){
 
- $('#button-menu').on('click', function (e){
+ $('#button-menu').on('click', function (){
 
   var  $this = $("#main");
 
-  if(!$this.hasClass('col-11')){
-    $("#aside").hide();
-    $this.addClass('col-11');
-    $this.removeClass('col-8');
+    if(!$this.hasClass('95-pecent')){
+      $("#aside").animate({left: '-285px'}, 300);
+      $("#aside").hide(200);
+      $this.addClass('95-pecent').css({width: '95%'});
 
-  } else {
-    $this.removeClass('col-11');
-    $this.addClass('col-8');
-    $("#aside").show(400);
-  }
-});
+    } else {
+      $this.removeClass('95-pecent').css({width: '82%'});
+      $("#aside").show();
+      $("#aside").animate({left: '0px'}, 1000);
+    }
+  });
 
-$(".img-post-footer").on("click", function () { // Отображение на весь экран
+$(".img-post-footer").on("click", function () {
   var img = $(this);
   var src = img.attr('src');
   $("#img-post-footer-big").attr('src', src);
   $("#modal-window").show();
 });
 
-$("#img-post-footer-big").on("click", function () { // Кнопка закрыть
+$("#img-post-footer-big").on("click", function () {
 	$("#modal-window").hide();
 });
 
